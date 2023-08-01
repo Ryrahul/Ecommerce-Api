4c2f82f8588f94fb35c465a205845c98a3dab44c
Rahul <caulirahul6@gmail.com>
2023-08-02 01:18:35 +0530

    Schema for Order

--- db/models/Category.Schema.js
+++ db/models/Category.Schema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  description: { type: String },
+  description: { type: String,required:false },
 });
 
 const Category = mongoose.model("Category", categorySchema);
