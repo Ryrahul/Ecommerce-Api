b7bd95c0cb5069c2b4a517cc58eb6c534edf1567
geekyharsh05 <hp424805@gmail.com>
2023-09-08 17:25:53 +0530

    :technologist: feat[Product]: Improved API endpoints consistency

--- src/controller/Product.js
+++ src/controller/Product.js
@@ -1,39 +1,58 @@
-const Product = require('../../db/models/productsSchema')
+const Product = require("../../db/models/productsSchema");
+
 const addProducts = async (req, res) => {
   try {
     const newProduct = new Product(req.body);
     await newProduct.save();
-    res.status(201).send(newProduct);
-  } catch (e) {
-    res.status(401).send(e.message);
+    res.status(201).json(newProduct); // Return the created product as JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to add product" });
   }
 };
+
 const getProducts = async (req, res) => {
   try {
     const allProducts = await Product.find();
-    res.status(200).send(allProducts);
-  } catch (e) {
-    res.status(401).send(e.message);
+    res.status(200).json(allProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve products" });
   }
 };
+
 const eachProduct = async (req, res) => {
   try {
     const { id } = req.params;
     const requiredProduct = await Product.findById(id);
-    res.status(200).send(requiredProduct);
-  } catch (e) {
-    res.status(401).send(e.message);
+
+    if (!requiredProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(requiredProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve product" });
   }
 };
+
 const deleteProduct = async (req, res) => {
   try {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
-    res.status(200).send(`deleted->>${deletedProduct}`);
-  } catch (e) {
-    res.status(401).send(e.message);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted", deletedProduct });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to delete product" });
   }
 };
+
 const updateProduct = async (req, res) => {
   try {
     const { id } = req.params;
@@ -48,21 +67,26 @@ const updateProduct = async (req, res) => {
     res.status(401).send(e.message);
   }
 };
+
 const categorizedProduct = async (req, res) => {
   CategoryId = req.params.CategoryId;
   console.log(CategoryId);
   try {
     const categoryProducts = await Product.find({ category: CategoryId });
-    if(!categoryProducts){
-        return res.status(401).send("no such products")
+    if (!categoryProducts || categoryProducts.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No products found in this category" });
     }
-    return res.status(200).send(categoryProducts);
-  } catch (e) {
-    res.status(401).send(e.message);
+    return res.status(200).json(categoryProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve category products" });
   }
 
   res.send("working");
 };
+
 module.exports = {
   addProducts,
   getProducts,
